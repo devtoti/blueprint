142b62f61138392e29badbd8cced27d39c2b77e5
Antonio <toti.webdev@gmail.com>
2025-02-18 21:38:01 -0600

    Add horizontal axes

---
+++ src/lib/components/HorizontalAxes.svelte
@@ -0,0 +1,63 @@
+<script>
+  let { width, height } = $props();
+</script>
+
+{#if width <= 480}
+  <ul>
+    <li>A</li>
+    <li>B</li>
+    <li>C</li>
+    <li>D</li>
+  </ul>
+{:else if width > 480 && width <= 720}
+  <ul>
+    <li>A</li>
+    <li>B</li>
+    <li>C</li>
+    <li>D</li>
+    <li>E</li>
+    <li>F</li>
+    <li>G</li>
+    <li>H</li>
+    <li>I</li>
+  </ul>
+{:else}
+  <ul>
+    <li>A</li>
+    <li>B</li>
+    <li>C</li>
+    <li>D</li>
+    <li>E</li>
+    <li>F</li>
+    <li>G</li>
+    <li>H</li>
+    <li>I</li>
+    <li>J</li>
+    <li>K</li>
+    <li>L</li>
+  </ul>
+{/if}
+
+<style>
+  ul {
+    grid-column: 1 / 13;
+    width: 100%;
+    display: contents;
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+
+  li {
+    flex: 1;
+    height: 1.5rem;
+    text-align: center;
+    outline: 1px solid gray;
+  }
+  ul:last-child {
+    grid-column-end: -1;
+  }
+  li:first-child {
+    grid-column-start: 1;
+  }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,45 @@
+<script>
+  import { onMount } from "svelte";
+  import "../styles.css";
+  import HorizontalAxes from "$lib/components/HorizontalAxes.svelte";
+  let width = $state(0);
+  let height = $state(0);
+  onMount(() => {
+    function onResize() {
+      width = document.documentElement.clientWidth;
+      height = document.documentElement.clientHeight;
+    }
+    window.addEventListener("resize", onResize);
+    onResize();
+    return () => {
+      window.removeEventListener("resize", onResize);
+    };
+  });
+</script>
+
+<main>
+  <HorizontalAxes {width} {height} />
+  <section class="hero">
+    <article>
+      <div>
+        <h1>
+          <span>{width}</span>
+          <span>{height}</span>
+        </h1>
+      </div>
+    </article>
+  </section>
+  <HorizontalAxes {width} {height} />
+</main>
+
+<style>
+  .hero {
+    height: calc(100svh - 1.5rem);
+    grid-column: 1 / -1;
+    grid-row: 2 / 3;
+    background-color: rgba(0, 0, 0, 0.1);
+  }
+  div {
+    background-color: lightgray;
+  }
+</style>
